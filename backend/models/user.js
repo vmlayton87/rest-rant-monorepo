522f11f4a04534a5a0ee7af154aef96d71635ce4
vmlayton87 <vmlayton87@gmail.com>
2024-04-23 18:48:16 -0400

    migration for password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
 
@@ -19,7 +18,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
+    passwordDigest: DataTypes.STRING
   }, {
     sequelize,
     underscored: true,
