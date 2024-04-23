9eac7769741e8aacde6b55e003250841ba9bf894
vmlayton87 <vmlayton87@gmail.com>
2024-04-23 19:46:16 -0400

    login page and usercontext

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -28,6 +28,13 @@ function LoginForm() {
 
         const data = await response.json()
 
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            history.push('/')
+        } else {
+            setErrorMessage(data.message)
+        }
+
         console.log(data)
     }
   
