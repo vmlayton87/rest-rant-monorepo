b199f0daeccdc75e1562c3421e6f80abfc4b8813
vmlayton87 <vmlayton87@gmail.com>
2024-04-28 10:53:02 -0400

    can login and get a token

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -13,9 +14,10 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId})
+        res.json({ user: user, token: result.value })
     }
-    req.session.userId=user.userId
+    // req.session.userId=user.userId
     console.log(user)
 })
 
